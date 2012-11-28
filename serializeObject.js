b8b82fd4d23852df38ac3e20a4c775d0a8a19abe
Jennifer Frankel <jennifer.u.frankel@gmail.com>
2012-11-28 09:40:07 -0800

    Cleaned up serializeObject

--- serializeObject.js
+++ serializeObject.js
@@ -1,17 +1,17 @@
 (function($){
 	$.fn.serializeObject = function(){
-		var a = $("#symptomsubmitform").find("[name]").map(function(i, el) {
-		var type = $(el).attr("type") || "text";
-		var o = {};
-		var val = $(el).val();
+		var a = this.find("[name]").map(function(i, el) {
+			var type = $(el).attr("type") || "text";
+			var o = {};
+			var val = $(el).val();
 
-		switch (type) {
-			case "checkbox" : val = !!val; break;
-			case "number" : val = parseFloat(val); break;
-			case "date" : val = new Date(val); break;
-		}
+			switch (type) {
+				case "checkbox" : val = !!val; break;
+				case "number" : val = parseFloat(val); break;
+				case "date" : val = new Date(val); break;
+			}
 
-		o[$(el).attr('name')] = val;
+			o[$(el).attr('name')] = val;
 			return o;
 		});
 		return _.reduce(a, function(a,b) {
@@ -19,3 +19,4 @@
 		});
 	};
 })(jQuery);
+
