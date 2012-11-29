425a185db3c5149d684c0c6813691a1d3e992f04
Jennifer Frankel <jennifer.u.frankel@gmail.com>
2012-11-28 22:43:02 -0800

    #1: Moved the fetching of the Parse data into the view

--- Views/CategoryListView.js
+++ Views/CategoryListView.js
@@ -2,14 +2,17 @@ var HypoApp = (typeof HypoApp === "undefined") ? {} : HypoApp;
 HypoApp.Views = (typeof HypoApp.Views === "undefined") ? {} : HypoApp.Views;
 
 HypoApp.Views.CategoryListView = Parse.View.extend({
-	initialize: function(options){
+	
+	initialize: function() {
 		_.bindAll(this);
-		this.collection = options.collection;
-		this.collection.on("all", this.render);
 		this.template = _.template($("#categoryList-template").html());
+		this.categories = new HypoApp.Models.Categories();
+		this.categories.on("all", this.render);
+		this.categories.fetch();
 	},
-	render: function(){
-		this.$el.html(this.template({categories: this.collection.toJSON()}));
+
+	render: function() {
+		this.$el.html(this.template({categories: this.categories.toJSON()}));
 		return this;
 	}
 });
