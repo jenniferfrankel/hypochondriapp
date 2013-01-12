617f7459fdd15769accb0fdcc13d2aded2a76712
Jennifer Frankel <jennifer.u.frankel@gmail.com>
2013-01-11 18:03:36 -0800

    Cleaned up some indentations

--- app/Views/ModalFormView.js
+++ app/Views/ModalFormView.js
@@ -1,43 +1,41 @@
 define(
 	["jquery", "parse", "underscore", "jquery.serializeobject"],
 	function($, Parse, _, template) {
-	return Parse.View.extend({
-		render: function(){
-			this.$el.html(this.template());
-			return this;
-		},
+		return Parse.View.extend({
+			render: function(){
+				this.$el.html(this.template());
+				return this;
+			},
 
-		toggleSubmitButtonDisabled: function(isDisabled) {
-			this.$("[type=submit]").prop('disabled', isDisabled);
-		},
+			toggleSubmitButtonDisabled: function(isDisabled) {
+				this.$("[type=submit]").prop('disabled', isDisabled);
+			},
 
-		hideModal: function() {
-			// Remove focus from input fields to ensure
-			// that keyboard field disappears when modal closes
-			this.$("input").blur();
-			$("#myModal").modal('hide');
-		},
+			hideModal: function() {
+				// Remove focus from input fields to ensure
+				// that keyboard field disappears when modal closes
+				this.$("input").blur();
+				$("#myModal").modal('hide');
+			},
 
-		onSendToParseComplete: function() {
-			this.toggleSubmitButtonDisabled(false);
-			this.hideModal();
-		},
+			onSendToParseComplete: function() {
+				this.toggleSubmitButtonDisabled(false);
+				this.hideModal();
+			},
 
-		validateForm: function() {
-			return true;
-		},
+			validateForm: function() {
+				return true;
+			},
 
-		handleSubmit: function(event){
-			event.preventDefault();
-			if(this.validateForm()) {
-				this.toggleSubmitButtonDisabled(true);
-
-				var data = this.getDataFromForm();
-				data.user = Parse.User.current();
-				data.ACL = new Parse.ACL(Parse.User.current());
-
-				this.saveToParse(data);
+			handleSubmit: function(event){
+				event.preventDefault();
+				if(this.validateForm()) {
+					this.toggleSubmitButtonDisabled(true);
+					var data = this.getDataFromForm();
+					data.user = Parse.User.current();
+					data.ACL = new Parse.ACL(Parse.User.current());
+					this.saveToParse(data);
+				}
 			}
-		}
+		});
 	});
-});
