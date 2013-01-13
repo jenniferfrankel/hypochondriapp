ce2f4359941fb1f2dacf1a26de07e194001d1787
Mattias Pierre <mattias.pierre@flixster-inc.com>
2013-01-12 22:20:11 -0800

    Added simple caching for categories

--- app/Views/CategoryListView.js
+++ app/Views/CategoryListView.js
@@ -1,4 +1,6 @@
-define(["jquery", "parse", "underscore", "../Models/Category", "text!../Templates/CategoryList.html", "./AddCategoryView", "spinhelper"], function($, Parse, _, Category, template, AddCategoryView, spinhelper) {
+define(
+	["jquery", "parse", "underscore", "../Models/Category", "text!../Templates/CategoryList.html", "./AddCategoryView", "spinhelper", "appCache"],
+	function($, Parse, _, Category, template, AddCategoryView, spinhelper, appCache) {
 	return Parse.View.extend({
 
 		pageTitle: "Symptoms",
@@ -7,36 +9,37 @@ define(["jquery", "parse", "underscore", "../Models/Category", "text!../Template
 			_.bindAll(this);
 			var that = this;
 			this.template = _.template(template);
-			var categoryQuery = new Parse.Query(Category);
-			categoryQuery.equalTo("user", Parse.User.current());
-			this.categories = categoryQuery.collection();
+			this.categories = this.fetchCategories();
 			this.categories.on("all", this.render);
-			var stopSpinner = function() {
-				$("#spinner").spin(false).hide();
-			};
+		},
 
-			$("#homeTab").removeClass("active");
-			$("#settingsTab").removeClass("active");
-			$("#symptomsTab").addClass("active");
-
+		showSpinner : function () {
 			$("#spinner").show().spin('large');
-			this.categories.fetch({
-				success: stopSpinner,
-				error: stopSpinner
-			});
+		},
 
-			this.categories.on("all", this.render);
-			this.hideBackButton = true;
-			$("#newButton").show();
-			$("#newButton").off("click");
-			$("#newButton").click(function(event) {
-				event.preventDefault();
-				that.addCategory();
-			});
+		stopSpinner : function () {
+			$("#spinner").spin(false).hide();
+		},
+
+		fetchCategories : function() {
+			var categories;
+			if (appCache.categories) {
+				categories = appCache.categories;
+			} else {
+				var categoryQuery = new Parse.Query(Category);
+				categoryQuery.equalTo("user", Parse.User.current());
+				categories = categoryQuery.collection();
+				this.showSpinner();
+				categories.fetch({
+					success: this.stopSpinner,
+					error: this.stopSpinner
+				});
+				appCache.categories = categories;
+			}
+			return categories;
 		},
 
 		render: function() {
-
 			this.$el.html(this.template({categories: this.categories.toJSON()}));
 			return this;
 		},
