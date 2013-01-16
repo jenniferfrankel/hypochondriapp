dba05bc731a28343569c153588b40f0ac6bb4fad
Jennifer Frankel <jennifer.u.frankel@gmail.com>
2013-01-16 12:02:44 -0800

    Added signup form validation

--- app/Views/SignUpView.js
+++ app/Views/SignUpView.js
@@ -16,19 +16,42 @@ define(["jquery", "parse", "underscore", "text!../Templates/Signup.html"], funct
 
 		signUp: function(e) {
 			e.preventDefault();
-			var that = this;
-			var email = this.$("#signup-email").val();
-			var username = this.$("#signup-username").val().toLowerCase();
-			var password = this.$("#signup-password").val();
-			this.$(".signup-form button").prop('disabled', true);
-			
-			Parse.User.signUp(username, password, { ACL: new Parse.ACL(), email: email }, {
-				success: this.success,
-				error: function(user, error) {
-					that.$(".signup-form .error").html(error.message).show();
-					that.$(".signup-form button").prop('disabled', false);
-				}
-			});
+			if(this.validateForm()){
+				var that = this;
+				var email = this.$("#signup-email").val();
+				var username = this.$("#signup-username").val().toLowerCase();
+				var password = this.$("#signup-password").val();
+				this.$(".signup-form button").prop('disabled', true);
+				
+				Parse.User.signUp(username, password, { ACL: new Parse.ACL(), email: email }, {
+					success: this.success,
+					error: function(user, error) {
+						that.$(".signup-form .error").html(error.message).show();
+						that.$(".signup-form button").prop('disabled', false);
+					}
+				});
+			}
+		},
+
+		validateForm: function() {
+			if (this.$("#signup-username").val().length < 1) {
+				alert("You need to choose a username.");
+				return false;
+			}
+			if (this.$("#signup-password").val().length < 1) {
+				alert("You need to choose a password.");
+				return false;
+			}
+			var regex = /^[\w\.\-\+]+\@([\w\-]+\.)+[a-zA-Z0-9]{2,4}$/;
+			if (!regex.test(this.$("#signup-email").val())) {
+				alert("You need to enter a valid email address");
+				return false;
+			}
+			if (!window.navigator.onLine) {
+				alert("Ooops! You seem to be offline. Please go online to sign up.");
+				return false;
+			}
+			return true;
 		},
 
 		onClickCancel: function() {
